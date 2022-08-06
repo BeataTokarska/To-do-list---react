33b4be2ac8f4cc3af12fa1e171ae3b62643f4501
Beata Tokarska <tokarskabb@gmail.com>
2022-08-06 18:00:21 +0200

    task listy without interactions yet

---
+++ src/Container/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+
+const Container = ({ children }) => (
+    <mian className="container">
+        {children}
+    </mian>
+);
+export default Container

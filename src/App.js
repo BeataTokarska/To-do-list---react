d6f4eabcb19d8562eee341064c143fbbead59534
Beata Tokarska <tokarskabb@gmail.com>
2022-09-21 11:30:21 +0200

    local storage odded

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -6,13 +6,25 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
+const getInitialTasks = () => {
+  const tasksFromLocalStorage = localStorage.getItem("tasks");
+  return tasksFromLocalStorage
+    ? JSON.parse(tasksFromLocalStorage)
+    : []
+};
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "iść na spacer", done: false },
-    { id: 2, content: "zjeść kolacje", done: true },
-  ]);
+  // const [tasks, setTasks] = useState([
+  //   { id: 1, content: "iść na spacer", done: false },
+  //   { id: 2, content: "zjeść kolacje", done: true },
+  // ]);
+
+  const [tasks, setTasks] = useState(getInitialTasks);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
@@ -25,7 +37,7 @@ function App() {
 
   const toggleTaskDone = (id) => {
     setTasks(tasks => tasks.map(task => {
-        if (task.id === id) {
+      if (task.id === id) {
         return { ...task, done: !task.done };
       }
       return task;
@@ -50,7 +62,7 @@ function App() {
   };
   return (
     <Container>
-      <Header title="Lista zadań" 
+      <Header title="Lista zadań"
       />
       <Section
         title="Dodaj nowe zadanie"
@@ -67,7 +79,7 @@ function App() {
             toggleTaskDone={toggleTaskDone}
           />
         }
-        
+
         extraHeaderContent={
           <Buttons
             tasks={tasks}
